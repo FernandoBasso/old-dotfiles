921c5f9894e5fc55d18375fb8e31f68026997d36
Fernando Basso <fernandobasso.br@gmail.com>
2016-09-11 07:34:00 -0300

    tmux-anki-studies script.

--- bin/tmux-studies-anki.bash
+++ bin/tmux-studies-anki.bash
@@ -3,25 +3,26 @@
 # Place where I manipulate files when I an studying my Anki cards
 # and need to run quick snippets of code to assert my knowledge of
 # the things I am committing to the long-term memory.
-temp_study_dir="${HOME}/Public/tempstudyfiles"
+dir="${HOME}/Public/tempstudyfiles"
+ses='anki-studies'
 
-if [ ! -d "$temp_study_dir" ] ; then
-    mkdir --parent "$temp_study_dir"
-    cd "$temp_study_dir"
+if [ ! -d "$dir" ] ; then
+    mkdir --parent "$dir"
+    cd "$dir"
 fi
 
 # \033 is \e, which is ESC.
 # \007 is \a, which is BEL.
-#echo -en '\033]2;Always Be Awesome!\007'
-printf '\e]2;%s\a' 'Always Be Awesome!'
+#echo -en '\033]2;Anki Studies!\007'
+printf '\e]2;%s\a' 'Anki Studies!'
 
-tmux new-session -d -s 'studies-anki' -c "$temp_study_dir"
-tmux rename-window 'vim' \; send-keys $'vim\n'
-tmux new-window -t studies-anki:1 -n 'emacs' \; send-keys $'emacs -nw\n'
-tmux new-window -t studies-anki:2 -n 'shell-1'
-tmux new-window -t studies-anki:3 -n 'shell-2'
-tmux new-window -t studies-anki:4 -n 'shell-3'
+tmux new-session -d -s "$ses" -c "$dir"
+tmux rename-window 'vim' \; send-keys $'vim \n'
+tmux new-window -t "${ses}:1" -n 'emacs' -c "$dir" \; send-keys $'emacs -nw\n'
+tmux new-window -t "${ses}:2" -n 'shell-1' -c "$dir"
+tmux new-window -t "${ses}:3" -n 'shell-2' -c "$dir"
+tmux new-window -t "${ses}:4" -n 'shell-3' -c "$dir"
 
-tmux select-window -t studies-anki:0
-tmux -2 attach-session -t studies-anki
+tmux select-window -t "${ses}:0"
+tmux -2 attach-session -t "${ses}"
 
