a7d41dffb0dbb1b2fd09de80dc7f43abc1462339
Fernando Basso <fernandobasso.br@gmail.com>
2016-10-09 18:12:04 -0300

    Several things configured in arch linux.

--- bin/tmux-pizza-site.bash
+++ bin/tmux-pizza-site.bash
@@ -6,13 +6,16 @@ ses='pizza-site'
 # Debian/Xubuntu
 apache_unit=apache2.service
 postgresql_unit=postgresql.service
+mariadb_unit=mysqld.unit
 
 # Archlinux machine (probably for Fedora and CentOS as well).
 if [ $HOSTNAME = theforce ] ; then
     apache_unit=httpd.service
+    mariadb_unit=mysqld.service
 fi
-services=($apache_unit $postgresql_unit)
 
+# Services we need to start for this project.
+services=($mariadb_unit)
 
 if [ ! -d "$dir" ] ; then
     echo "Directory \`$dir\` not found."
@@ -22,12 +25,11 @@ if [ ! -d "$dir" ] ; then
 fi
 
 for service in "${services[@]}" ; do
-    echo $service
     if systemctl --quiet is-active $service = 0 ; then
-	echo "${service} is already running."
+        echo "${service} is already running."
     else
-	echo "Starting ${service}..."
-	sudo systemctl start $service
+        echo "Starting ${service}..."
+        sudo systemctl start $service
     fi
 done
 
@@ -35,20 +37,23 @@ done
 # NOTE: base-index is set to 1 (relevant for the window numbers in commands).
 
 tmux new-session -d -s "$ses" -c "$dir" \; rename-window 'vim' \; \
-    send-keys 'vim app.php' C-m
+    send-keys 'vim src/AppBundle/Controller/HomeController.php' C-m
 
 # php -S takes a host name, without the protocol:// part.
-tmux new-window -t "${ses}:2" -n 'php -S' -c "$dir" \; \
-    send-keys 'php -S dev.local.me:8080' C-m
+tmux new-window -t "${ses}:2" -n 'server' -c "$dir" \; \
+    send-keys 'php bin/console server:run' C-m
 
 tmux new-window -t "${ses}:3" -n 'sass' -c "$dir" \; \
-    send-keys 'sass --watch scss:css' C-m
+    send-keys 'sass --watch web/scss/:web/css/' C-m
 
-tmux new-window -t "${ses}:4" -n 'git' -c "$dir" \; \
+tmux new-window -t "${ses}:4" -n "logs" -c "$dir" \; \
+    send-keys 'tail -f var/logs/dev.log' C-m
+
+tmux new-window -t "${ses}:5" -n 'git' -c "$dir" \; \
     send-keys 'git branch ; git status' C-m
 
 # With LC_COLLATE=C we list dot files first.
-tmux new-window -t "${ses}:5" -n 'shell' -c "$dir" \; \
+tmux new-window -t "${ses}:6" -n 'shell' -c "$dir" \; \
     send-keys 'LC_COLLATE=C \ls --color=auto --format=long --human-readable --almost-all --classify' C-m
 
 tmux select-window -t "${ses}:1"
